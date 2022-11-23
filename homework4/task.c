bc12f8676f1fc84dcb28fe633ad4b3aa9bcf29db
Henry <thbulluck@gmail.com>
2022-11-22 20:11:56 -0600

    fixed double digit error. time isn't improved believe its part of hardware not software

--- homework4/task.c
+++ homework4/task.c
@@ -64,29 +64,32 @@ void* worker_thread_function(void *tinput_void){
         if(try_solution(tinput->challenge, attempted_solution)){
             //condition2: the last digit must be different in all the solutions
             short bad_solution = 0;
-            pthread_rwlock_rdlock(&sol); //4
+            //pthread_rwlock_wrlock(&sol); //4
             for(int i=0;i<found_solutions;i++){
-            	//pthread_rwlock_rdlock(&sol); //5
+            	pthread_rwlock_rdlock(&sol); //5
                 if(attempted_solution%10 == solutions[i]%10){
                     bad_solution = 1;
                 }
-                //pthread_rwlock_unlock(&sol); //5
+                pthread_rwlock_unlock(&sol); //5
             }
-            pthread_rwlock_unlock(&sol); //4
+            //pthread_rwlock_unlock(&sol); //4
+            pthread_rwlock_wrlock(&sol); //4
             if(bad_solution){
+            	pthread_rwlock_unlock(&sol);
                 continue;
             }
 
             //condition3: no solution should be divisible by any number in the range [1000000, 1500000]
             if(!divisibility_check(attempted_solution)){
+            	pthread_rwlock_unlock(&sol);
                 continue;
             }
-            pthread_rwlock_rdlock(&sol); //1
+            //pthread_rwlock_rdlock(&sol); //1
             if(found_solutions==NSOLUTIONS){
             	pthread_rwlock_unlock(&sol);
                 return NULL;
             }
-            pthread_rwlock_unlock(&sol); //1
+            //pthread_rwlock_unlock(&sol); //1
             
             pthread_rwlock_wrlock(&sol); //2
             solutions[found_solutions] = attempted_solution;
